898f8f4065bad71b07ab0ef330034c1bc310b8ff
Andreas Ekdahl <andy@ekdahls.net>
2021-06-22 09:37:35 +0200

    Added backend

--- src/Karlstad.Edu.Web/client/components/KarlstadTest.tsx
+++ src/Karlstad.Edu.Web/client/components/KarlstadTest.tsx
@@ -3,21 +3,32 @@ import { vueCustomElement, IWebComponentInstance, WebComponentBootstrapper } fro
 import { StyleFlow, VueComponentBase, BlockSettingsReader } from '@omnia/fx/ux';
 import { KarlstadTestStyles } from './KarlstadTest.css';
 import { KarlstadTestBlockSettings } from './KarlstadTestSettings';
+import {Inject, HttpClient, HttpClientConstructor}  from '@omnia/fx';
 
 @Component
 export default class KarlstadTest extends VueComponentBase implements IWebComponentInstance {
     
 
     private KarlstadTestClasses = StyleFlow.use(KarlstadTestStyles);
-
+    private demoContent: string = "";
+    
     @BlockSettingsReader<KarlstadTestBlockSettings>({
         defaultValue: { title: 'Hello Karlstad',description:'default beskrivning'},
         editElement: "kstad-omnia-test-settings"
     })
     protected settings: KarlstadTestBlockSettings;
 
-    mounted() {
+
+    @Inject<HttpClientConstructor>(HttpClient, {
+        configPromise: HttpClient.createOmniaServiceRequestConfig('7433f149-a76b-4d30-9d14-503dc66d5de8')
+    }) private httpClient: HttpClient
+
+
+    async mounted() {
         WebComponentBootstrapper.registerElementInstance(this, this.$el);
+
+        let res = await this.httpClient.get('/api/demo');
+        this.demoContent = res.data;
     }
 
     render(h) {
@@ -25,6 +36,7 @@ export default class KarlstadTest extends VueComponentBase implements IWebCompon
             <div class={this.KarlstadTestClasses.container}>
                 {this.settings.title}
                 {this.settings.description}
+                {this.demoContent}
             </div>
         )
     }
