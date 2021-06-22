d094d8148bc7a52087cdc06abda582c4233fbca5
David Jönell <david_jonell@hotmail.com>
2021-06-22 09:37:47 +0200

    added solveig

--- src/Karlstad.Edu.Web/client/components/KarlstadTest.tsx
+++ src/Karlstad.Edu.Web/client/components/KarlstadTest.tsx
@@ -15,17 +15,55 @@ export default class KarlstadTest extends VueComponentBase implements IWebCompon
         editElement: "kstad-omnia-test-settings"
     })
     protected settings: KarlstadTestBlockSettings;
+    private sliderValue:number = 25;
+    private dialogModel = false;
 
     mounted() {
         WebComponentBootstrapper.registerElementInstance(this, this.$el);
     }
 
+
+
+
     render(h) {
         return (
-            <div class={this.KarlstadTestClasses.container}>
-                {this.settings.title}
-                {this.settings.description}
-            </div>
+            <v-row>
+                <v-col cols="6" >
+                    <v-card>
+                        <v-card-title>          
+                            <v-text-field
+                        filled
+                        v-model={this.settings.title}
+                        label='Title'></v-text-field>
+                        </v-card-title>
+                        <v-card-text>text
+                        <v-slider
+                            hint="Im a hint"
+                            max="50"
+                            v-model={this.sliderValue}
+                            min="-50"
+                            ></v-slider>                            
+                        </v-card-text>
+                        SliderValue är {this.sliderValue}
+                    </v-card>
+                </v-col>
+                <v-col cols="4" >
+                <v-btn
+                    text="ClickMe"
+                    onClick={()=>this.dialogModel = true}
+                >
+                    clickme
+                </v-btn>
+                </v-col>
+                <v-col cols="2" >
+                <v-dialog
+                    v-model={this.dialogModel}
+                    width="500"
+                    >
+                        dialogtext
+                        </v-dialog>
+                </v-col>
+            </v-row>
         )
     }
 }
