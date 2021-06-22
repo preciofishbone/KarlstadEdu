66569e95597aa3dea570ad06420de51e954b6c9e
Andreas Ekdahl <andy@ekdahls.net>
2021-06-22 13:11:58 +0200

    Added backend service for Solveig

---
+++ src/Karlstad.Edu.Web/Models/SolveigItem.cs
@@ -0,0 +1,15 @@
+
+namespace Karlstad.Edu.Web.Models
+{
+
+    public class SolveigItem
+    {
+
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string Summary { get; set; }
+        public string ThumbnailURL { get; set; }
+        public string FullURL { get; set; }
+
+    }
+}
